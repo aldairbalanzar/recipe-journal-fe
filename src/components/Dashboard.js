2315a4ca9dc6e56546268f970f06fed19bbf8d61
aldairbalanzar <abalanzar7@gmail.com>
2020-10-04 18:42:57 -0400

    started search bar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import AddRecipeForm from './AddRecipeForm';
 import RecipeCard from './RecipeCard';
-import { RaisedButton } from 'material-ui';
+import { TextField, RaisedButton } from 'material-ui';
 import { getRecipes } from '../actions/recipesActions';
 import { connect } from 'react-redux';
 
 const Dashboard = (props) => {
 
     const [isCreatingRecipe, setIsCreatingRecipe] = useState(false);
+    const [search, setSearch] = useState('')
+
+    const handleSearchChanges = e => {
+        e.preventDefault()
+        setSearch(e.target.value)
+    };
+
+    const handleSearchSubmit = e => {
+        e.preventDefault()
+        console.log(search)
+    }
 
     const toggleCreateRecipeForm = () => {
         setIsCreatingRecipe(!isCreatingRecipe)
@@ -17,8 +28,25 @@ const Dashboard = (props) => {
         props.getRecipes(props.userData.id)
     }, [props.userData]);
 
+    useEffect(() => {
+        console.log('hey')
+    }, [search])
+
     return (
         <div className='dashboard'>
+            <div className='search-container'>
+                <TextField 
+                className='input-field'
+                type='text'
+                onChange={handleSearchChanges}
+                id='stepNum'
+                name='stepNum'
+                value={search}
+                placeholder='Search'
+                floatingLabelText='Search'
+                />
+                <RaisedButton onClick={handleSearchSubmit}>Search</RaisedButton>
+            </div>
             <div className='user-data-container'>
                 <h3 className='user-id'>id: {props.userData.id}</h3>
                 <h3 className='username'>User: {props.userData.username}</h3>
