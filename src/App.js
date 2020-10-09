d4eb54d62855c6a21bda8f94e28bbd2ee4c8b3b3
aldairbalanzar <abalanzar7@gmail.com>
2020-10-09 03:19:25 -0400

    added onunload function to clear local storage upon tab close

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Beforeunload } from 'react-beforeunload';
 import Register from './components/Register';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
@@ -11,29 +12,30 @@ import './App.scss';
 
 function App() {
 
-  const [navState, setNavState] = useState(2)
-
-  const [userData, setUserData] = useState({
-    id: '',
-    username: ''
-})
+  const handleOnUnload = () => {
+    window.localStorage.setItem('token', JSON.stringify(''));
+    window.localStorage.setItem('id', JSON.stringify(''))
+    window.localStorage.setItem('username', JSON.stringify(''))
+  }
 
   return (
-    <Router>
-      <div className='app'>
-        <MuiThemeProvider>
-          <Nav setNavState={setNavState} />
-          <div className='app-content-container'>
-            <Switch>
-              <Route path='/register' component={Register} />
-              <Route path='/' exact component={Login} />
-              <Route path='/dashboard' component={Dashboard} />
-              <Route path='/selected' component={CurrentRecipe} />
-            </Switch>
-          </div>
-        </MuiThemeProvider>
-      </div>
-    </Router>
+    <Beforeunload onBeforeunload={handleOnUnload}>
+      <Router>
+        <div className='app'>
+          <MuiThemeProvider>
+            <Nav />
+            <div className='app-content-container'>
+              <Switch>
+                <Route path='/register' component={Register} />
+                <Route path='/' exact component={Login} />
+                <Route path='/dashboard' component={Dashboard} />
+                <Route path='/selected' component={CurrentRecipe} />
+              </Switch>
+            </div>
+          </MuiThemeProvider>
+        </div>
+      </Router>
+    </Beforeunload>
   );
 }
 
